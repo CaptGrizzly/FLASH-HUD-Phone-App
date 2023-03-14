ee5a4aa508e0c33cb194d95dbfc594ab1d918616
BraydenWalker <78127580+BraydenWalker@users.noreply.github.com>
2023-03-14 10:28:57 -0700

    first commit

---
+++ flash_app/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        primarySwatch: Colors.lightBlue,
+      ),
+      home: home(),
+    );
+  }
+}
---
+++ flash_app/lib/newFlight.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+import 'options.dart';
+
+class newFlight extends StatefulWidget {
+  @override
+  _newFlight createState() => _newFlight();
+}
+
+class _newFlight extends State<newFlight> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Container(
+            child: Column(
+              children: [
+                Text("Enjoy",style: TextStyle(fontSize: 30),),
+                ElevatedButton(onPressed: () {
+                  Navigator.pop(context);
+                },
+                  child: Text("Go back"),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
