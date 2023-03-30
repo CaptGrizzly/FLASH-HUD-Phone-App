ad007c6f438466aede3da633c62c1ca0075213f3
Myeongkee9 <gee.myeong@gmail.com>
2023-03-30 11:48:08 -0700

    Bluetooth

--- flash_app/lib/home.dart
+++ flash_app/lib/home.dart
@@ -1,4 +1,9 @@
+import 'dart:async';
+import 'dart:io' show Platform;
+
 import 'package:flutter/material.dart';
+import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
+import 'package:location_permissions/location_permissions.dart';
 
 import 'newFlight.dart';
 import 'options.dart';
@@ -10,6 +15,87 @@ class home extends StatefulWidget {
 
 class _homeState extends State<home> {
   dynamic size,height,width;
+  bool foundDevice = false;
+  bool scanStarted = false;
+  bool connected = false;
+// Bluetooth related variables
+  late DiscoveredDevice uniqueDevice;
+  final flutterReactiveBle = FlutterReactiveBle();
+  late StreamSubscription<DiscoveredDevice> _scanStream;
+  late QualifiedCharacteristic _rxCharacteristic;
+// These are the UUIDs of your device
+  final Uuid serviceUuid = Uuid.parse("75C276C3-8F97-20BC-A143-B354244886D4");
+  final Uuid characteristicUuid = Uuid.parse("6ACF4F08-CC9D-D495-6B41-AA7E60C4E8A6");
+
+  void startScan() async {
+// Platform permissions handling stuff
+    bool permGranted = false;
+    setState(() {
+      scanStarted = true;
+    });
+    PermissionStatus permission;
+    if (Platform.isAndroid) {
+      permission = await LocationPermissions().requestPermissions();
+      if (permission == PermissionStatus.granted) permGranted = true;
+    } else if (Platform.isIOS) {
+      permGranted = true;
+    }
+// Main scanning logic happens here ⤵️
+    if (permGranted) {
+      _scanStream = flutterReactiveBle
+          .scanForDevices(withServices: [serviceUuid]).listen((device) {
+        // Change this string to what you defined in Zephyr
+        if (device.name == 'UNIQUE') {
+          setState(() {
+            uniqueDevice = device;
+            foundDevice = true;
+          });
+        }
+      });
+    }
+  }
+
+  void connectToDevice() {
+    _scanStream.cancel();
+    // Let's listen to our connection so we can make updates on a state change
+    Stream<ConnectionStateUpdate> currentConnectionStream = flutterReactiveBle
+        .connectToAdvertisingDevice(
+        id: uniqueDevice.id,
+        prescanDuration: const Duration(seconds: 1),
+        withServices: [serviceUuid, characteristicUuid]);
+    currentConnectionStream.listen((event) {
+      switch (event.connectionState) {
+      // We're connected and good to go!
+        case DeviceConnectionState.connected:
+          {
+            _rxCharacteristic = QualifiedCharacteristic(
+                serviceId: serviceUuid,
+                characteristicId: characteristicUuid,
+                deviceId: event.deviceId);
+            setState(() {
+              foundDevice = false;
+              connected = true;
+            });
+            break;
+          }
+      // Can add various state state updates on disconnect
+        case DeviceConnectionState.disconnected:
+          {
+            break;
+          }
+        default:
+      }
+    });
+  }
+
+  void partyTime() {
+    if (connected) {
+      flutterReactiveBle
+          .writeCharacteristicWithResponse(_rxCharacteristic, value: [
+        0xff,
+      ]);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -70,6 +156,63 @@ class _homeState extends State<home> {
           ),
         ),
       ),
+      persistentFooterButtons: [
+        // We want to enable this button if the scan has NOT started
+        // If the scan HAS started, it should be disabled.
+        scanStarted
+        // True condition
+            ? ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            primary: Colors.grey, // background
+            onPrimary: Colors.white, // foreground
+          ),
+          onPressed: () {},
+          child: const Icon(Icons.search),
+        )
+        // False condition
+            : ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            primary: Colors.blue, // background
+            onPrimary: Colors.white, // foreground
+          ),
+          onPressed: startScan,
+          child: const Icon(Icons.search),
+        ),
+        foundDevice
+        // True condition
+            ? ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            foregroundColor: Colors.white, primary: Colors.blue, // foreground
+          ),
+          onPressed: connectToDevice,
+          child: const Icon(Icons.bluetooth),
+        )
+        // False condition
+            : ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            foregroundColor: Colors.white, primary: Colors.grey, // foreground
+          ),
+          onPressed: () {},
+          child: const Icon(Icons.bluetooth),
+        ),
+        connected
+        // True condition
+            ? ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            foregroundColor: Colors.white, primary: Colors.blue, // foreground
+          ),
+          onPressed: partyTime,
+          child: const Icon(Icons.celebration_rounded),
+        )
+        // False condition
+            : ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            foregroundColor: Colors.white, primary: Colors.grey, // foreground
+          ),
+          onPressed: () {},
+          child: const Icon(Icons.celebration_rounded),
+        ),
+      ],
     );
   }
 }
