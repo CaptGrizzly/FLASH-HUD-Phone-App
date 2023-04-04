30cdae8fcc4873dcdcaca6e772c286167719a5fd
Myeongkee9 <gee.myeong@gmail.com>
2023-04-04 10:22:48 -0700

    Device sensors

--- flash_app/lib/newFlight.dart
+++ flash_app/lib/newFlight.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 import 'home.dart';
@@ -11,31 +13,118 @@ class newFlight extends StatefulWidget {
 }
 
 class _newFlight extends State<newFlight> {
-  dynamic x, y, z;
+
   List<double>? _accelerometerValues;
   List<double>? _userAccelerometerValues;
   List<double>? _gyroscopeValues;
   List<double>? _magnetometerValues;
+  final _streamSubscriptions = <StreamSubscription<dynamic>>[];
+
   @override
   Widget build(BuildContext context) {
-    dynamic accelerometer =
+    final accelerometer =
     _accelerometerValues?.map((double v) => v.toStringAsFixed(1)).toList();
-    dynamic gyroscope =
+    final gyroscope =
     _gyroscopeValues?.map((double v) => v.toStringAsFixed(1)).toList();
-    dynamic userAccelerometer =
-    _userAccelerometerValues?.map((double v) => v.toStringAsFixed(1)).toList();
-    dynamic magnetometer =
+    final userAccelerometer = _userAccelerometerValues
+        ?.map((double v) => v.toStringAsFixed(1))
+        .toList();
+    final magnetometer =
     _magnetometerValues?.map((double v) => v.toStringAsFixed(1)).toList();
+
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Text('Accelerometer: $accelerometer'),
-            Text('UserAccelerometer: $userAccelerometer'),
-            Text('Gyroscope: $gyroscope'),
-            Text('Magnetometer: $magnetometer')
-          ],
-        ),
+      appBar: AppBar(
+        title: const Text('Sensor Example'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text('Accelerometer: $accelerometer'),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text('UserAccelerometer: $userAccelerometer'),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text('Gyroscope: $gyroscope'),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text('Magnetometer: $magnetometer'),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    for (final subscription in _streamSubscriptions) {
+      subscription.cancel();
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _streamSubscriptions.add(
+      accelerometerEvents.listen(
+            (AccelerometerEvent event) {
+          setState(() {
+            _accelerometerValues = <double>[event.x, event.y, event.z];
+          });
+        },
+      ),
+    );
+    _streamSubscriptions.add(
+      gyroscopeEvents.listen(
+            (GyroscopeEvent event) {
+          setState(() {
+            _gyroscopeValues = <double>[event.x, event.y, event.z];
+          });
+        },
+      ),
+    );
+    _streamSubscriptions.add(
+      userAccelerometerEvents.listen(
+            (UserAccelerometerEvent event) {
+          setState(() {
+            _userAccelerometerValues = <double>[event.x, event.y, event.z];
+          });
+        },
+      ),
+    );
+    _streamSubscriptions.add(
+      magnetometerEvents.listen(
+            (MagnetometerEvent event) {
+          setState(() {
+            _magnetometerValues = <double>[event.x, event.y, event.z];
+          });
+        },
       ),
     );
   }
